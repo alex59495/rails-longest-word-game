6694d21036d68bfcb9772566cda1d6d477413d37
Alexis Lenoir <alexis59495@gmail.com>
2020-04-16 22:06:42 -0300

    Give the score of the wird

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+# require 'open-uri'
+# require 'json'
+
 class GamesController < ApplicationController
     def new
         @letters = []
@@ -8,6 +11,31 @@ class GamesController < ApplicationController
     end
 
     def score
-        raise
+        # binding.pry
+        url = open("https://wagon-dictionary.herokuapp.com/#{params[:word]}").read
+        @hash_word = JSON.parse(url)
+        valid_letter = true
+        @letters = params[:letters].gsub(/[^a-zA-Z,]/, "").split(',')
+        @hash_word["word"].split("").each do |letter|
+            if @letters.include? letter.upcase
+                @valid_letter = true
+            elsif
+                @valid_letter = false
+            break if @valid_letter == false
+            end
+        end
+        if @valid_letter == false
+            @answer = "Sorry but <strong>#{params[:word].upcase}</strong> can't be build with #{@letters}." 
+        elsif @hash_word["found"] == false
+            @answer = "Sorry but <strong>#{params[:word].upcase}</strong> doesn't seem to be a valid English word..."
+        else
+           @answer = "<strong>Congratulation !</strong> #{params[:word].upcase} is a valid english word."
+           if session[:score] 
+            session[:score] += params[:word].length
+           else
+            session[:score] = params[:word].length
+           end
+        end
+        
     end
 end
